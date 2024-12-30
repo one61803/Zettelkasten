ae41f0764dfd728c176eb6698b659eec7486206b
one61803 <92607101+one61803@users.noreply.github.com>
2024-12-29 18:59:52 -0600

    correction/simplification

--- trigram.el
+++ trigram.el
@@ -1,23 +1,6 @@
 (require 'ido)
 
-(defun trigram (syll)
-    "Inserts a bagua trigram."
-    (interactive "sName? ")
-    (cond
-        ((string= syll "QIAN") (insert "☰"))
-        ((string= syll "KUN") (insert "☷"))
-	((string= syll "LI") (insert "☲"))
-	((string= syll "KAN") (insert "☵"))
-	((string= syll "DUI") (insert "☱"))
-	((string= syll "XUN") (insert "☴"))
-	((string= syll "ZHEN") (insert "☳"))
-	((string= syll "GEN") (insert "☶"))
-    )
-    )
-; (describe-function 'trigram)
-
-; -> [[switch statements]]
-; Reference: http://xahlee.info/emacs/emacs/elisp_idioms_prompting_input.html
+; Reference: http://xahlee.info/emacs/emacs/elisp_get_user_input.html
 
 (defun trigram ()
     "Use the left or right arrow to choose from among the choices (eight trigrams)."
@@ -38,6 +21,3 @@
     )
     )
 ; (describe-function 'trigram)
-
-;☳
-;☵
