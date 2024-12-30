2c41589e8733ef45b5cc6be5440a1b011dfc5a1e
one61803 <92607101+one61803@users.noreply.github.com>
2024-12-30 02:49:36 -0600

    Create start.el

---
+++ start.el
@@ -0,0 +1,15 @@
+(load-file "Zettel.el")
+(load-file "org-mode.el")
+(load-file "characters.el")
+
+(global-visual-line-mode)
+(org-mode)
+
+(defun ID0 ()
+   "This is a way of exiting the M-x line."
+   (interactive)
+   (message "logicinaction.org/docs/ch6.pdf ε")
+   )
+; (describe-function 'ID0)
+
+(setq phi 1.6180339887)
