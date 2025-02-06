e9c2dee6b21f472e70675b3a0a46236cf10e85dd
one61803 <92607101+one61803@users.noreply.github.com>
2025-02-05 18:24:39 -0600

    abbreviations:

--- org-mode.el
+++ org-mode.el
@@ -15,42 +15,42 @@
 ; (describe-function 'lk)
 
 (defun ASCII-link (name)
-  "Inserts a link to a file whose title is ASCII-encoded."
+  "Inserts a link to a file whose title is ASCII-encoded. (An abbreviation is 'AS-li'.)"
   (interactive "sEnter filename:")
   (insert (concat "[[./" (ASCII-trans-0 name) ".org][" (ASCII-trans-0 name) "]]"))
   )
 ; (describe-function 'ASCII-link)
 
 (defun ASCII-lk (name)
-  "Synonym of ASCII-link. (An abbreviation is 'AS-li'.)"
+  "Synonym of ASCII-link. (An abbreviation is AS-lk.)"
   (interactive "sEnter filename:")
   (insert (concat "[[./" (ASCII-trans-0 name) ".org][" (ASCII-trans-0 name) "]]"))
   )
 ; (describe-function 'ASCII-lk)
 
 (defun ASCII-link-2 (name)
-  "Like ASCII-link but leaving spaces as they are (unencoded)."
+  "Like ASCII-link but leaving spaces as they are (unencoded). (An abbreviation is 'AS-li-2'.)"
   (interactive "sEnter filename:")
   (insert (concat "[[./" (ASCII-trans-2-0 name) ".org][" (ASCII-trans-2-0 name) "]]"))
   )
 ; (describe-function 'ASCII-link)
 
 (defun ASCII-lk-2 (name)
-  "Synonym of ASCII-link-2. (An abbreviation is 'AS-li-2'.)"
+  "Synonym of ASCII-link-2. (An abbreviation is AS-lk-2.)"
   (interactive "sEnter filename:")
   (insert (concat "[[./" (ASCII-trans-2-0 name) ".org][" (ASCII-trans-2-0 name) "]]"))
   )
 ; (describe-function 'ASCII-lk)
 
 (defun el-link (name)
-  "Create a link to an Emacs Lisp file."
+  "Create a link to an Emacs Lisp file. (An abbreviation is 'el-li'.)"
   (interactive "sEnter filename:")
   (insert (concat "[[./" name ".el][" name ".el]]"))
   )
 ; (describe-function 'el-link)
 
 (defun el-lk  (name)
-  "Synonym of el-link. (An abbreviation is 'el-li'.)"
+  "Synonym of el-link."
   (interactive "sEnter filename:")
   (insert (concat "[[./" name ".el][" name ".el]]"))
   )
@@ -58,7 +58,7 @@
 
 
 (defun index-link (name)
-  "Create an org-mode link from the index."
+  "Create an org-mode link from the index. (An abbreviation is index-l.)"
   (interactive "sEnter filename: ")
   (insert (concat "[[./" name ".org][." name "]]"))
   )
@@ -72,7 +72,9 @@
 ; (describe-function 'ix-lk)
 
 (defun targ-link (name)
-  "Link to a Zettel that has an anchor made through :PROPERTIES: / :CUSTOM_ID: section / :END: drawer. (May use 'prop' function in Zettel.el or 'anchor' function in this file to create that properties drawer.)"
+  """Link to a Zettel that has an anchor made through :PROPERTIES: / :CUSTOM_ID: section / :END: drawer. 
+	(May use 'prop' function in Zettel.el or 'anchor' function in this file to create that properties drawer.)
+	(Some abbreviations are tar-li or tar-l.)"""
   (interactive "sEnter filename: ")
   (let (section)
     (setq section (read-string "Enter section: "))
